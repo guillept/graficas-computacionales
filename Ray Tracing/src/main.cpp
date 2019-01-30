125210b1b5effb893b01b18146e0e38910aefd92
Guille Pérez <guillermopt@outlook.com>
2019-01-30 16:26:07 -0600

    Capitulo 6

--- Ray Tracing/src/main.cpp
+++ Ray Tracing/src/main.cpp
@@ -4,6 +4,7 @@
 #include "sphere.h"
 #include "hitable_list.h"
 #include "float.h"
+#include "camera.h"
 #define MAXFLOAT    3.37E+38
 
 // #include "vec3.h"
@@ -46,29 +47,38 @@ int main(){
 
 	int nx = 200;
 	int ny = 100;
+	int ns = 100;
+	// std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
 	std::vector <unsigned char> pixels; //0 - 255 
 
-	vec3 lower_left_corner(-2.0, -1.0, -1.0);
+	/* vec3 lower_left_corner(-2.0, -1.0, -1.0);
 	vec3 horizontal(4.0, 0.0, 0.0);
 	vec3 vertical(0.0, 2.0, 0.0);
-	vec3 origin(0.0, 0.0, 0.0);
-
-	// std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+	vec3 origin(0.0, 0.0, 0.0); */
 
 	hitable *list[2];
 	list[0] = new sphere(vec3(0, 0, -1), 0.5);
 	list[1] = new sphere(vec3(0, -100.5, -1), 100);
 	hitable *world = new hitable_list(list, 2);
-
+	camera cam;
 	for (int j=ny-1; j>=0; j--){
 		for (int i=0; i<nx; i++){
+			vec3 col(0, 0, 0);
+			for(int s=0; s < ns; s++) {
+				float u = float(i + float(rand())/RAND_MAX) / float(nx);
+				float v = float(j + float(rand())/RAND_MAX) / float(ny);
+				ray r = cam.get_ray(u, v);
+				vec3 p = r.point_at_parameter(2.0);
+				col += color(r, world);
+			}
 
-			float u = float(i) / float(nx);
+			col /= float(ns);
+			/* float u = float(i) / float(nx);
 			float v = float(j) / float(ny);
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-			vec3 p = r.point_at_parameter(2.0);
-			vec3 col = color(r, world);
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical);*/
+
+			// vec3 col = color(r, world);
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
 			int ib = int(255.99 * col[2]);
