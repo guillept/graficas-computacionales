d5e7bf435bca7f3393f62b008d7054baf7139678
Guille Pérez <guillermopt@outlook.com>
2019-01-30 20:46:34 -0600

    Capitulo 9

--- Ray Tracing/src/main.cpp
+++ Ray Tracing/src/main.cpp
@@ -68,13 +68,20 @@ int main(){
 	vec3 vertical(0.0, 2.0, 0.0);
 	vec3 origin(0.0, 0.0, 0.0); */
 
-	hitable *list[4];
-	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3 (0.8, 0.3, 0.3)));
+	hitable *list[5];
+	/* list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3 (0.8, 0.3, 0.3)));
 	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
 	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
 	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8)));
+	*/
 
-	hitable *world = new hitable_list(list, 4);
+	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
+	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
+	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
+	list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
+
+	hitable *world = new hitable_list(list, 5);
 	camera cam;
 	for (int j=ny-1; j>=0; j--){
 		for (int i=0; i<nx; i++){
